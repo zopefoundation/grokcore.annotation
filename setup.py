b8114c3a8889d4aa3c0cd63abc8f1303f654093c
Sylvain Viollon <thefunny@gmail.com>
2009-06-15 16:09:05 +0000

    Skeleton for grokcore.annotation.

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    )
+
+setup(
+    name='grokcore.annotation',
+    version='1.0dev',
+    author='Grok Team',
+    author_email='grok-dev@zope.org',
+    url='http://grok.zope.org',
+    download_url='http://pypi.python.org/pypi/grokcore.annotation',
+    description='Grok-like configuration for Zope annotations',
+    long_description=long_description,
+    license='ZPL',
+    classifiers=['Environment :: Web Environment',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: Zope Public License',
+                 'Programming Language :: Python',
+                 'Framework :: Zope3',
+                 ],
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['grokcore'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools',
+                      'martian',
+                      'zope.component',
+                      'zope.interface',
+                      'grokcore.component',
+                      # for ftests:
+                      'zope.app.zcmlfiles',
+                      ],
+)
