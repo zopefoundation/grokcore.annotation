d97844cef9069e9229cc2cf0f2deaedc618eea21
Sylvain Viollon <thefunny@gmail.com>
2009-09-18 08:36:57 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='grokcore.annotation',
-    version='1.1dev',
+    version='1.2dev',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
