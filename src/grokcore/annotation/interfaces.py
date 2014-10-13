ec61a5291b0a7181bf0858bdbeebb29ad23c51ff
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2014-10-13 21:47:02 +0200

    Update some of the wording.

--- src/grokcore/annotation/interfaces.py
+++ src/grokcore/annotation/interfaces.py
@@ -28,7 +28,8 @@ class IAnnotationFactory(interface.Interface):
     name = interface.Attribute('Name of the annotation')
 
     def get(context):
-        """Return the existing annotation or None if None exists.
+        """Return the existing annotation or None if no annotaion
+        was created before.
         """
 
     def __call__(context):
