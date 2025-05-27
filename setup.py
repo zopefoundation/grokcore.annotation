5ea263d85f67a6aa49fc1dbdd77e26e986780897
Michael Howitz <icemac@gmx.net>
2025-05-27 08:22:14 +0200

    Preparing release 4.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ tests_require = [
 
 setup(
     name='grokcore.annotation',
-    version='4.1.dev0',
+    version='4.1',
     author='Grok Team',
     author_email='zope-dev@zope.dev',
     url='https://github.com/zopefoundation/grokcore.annotation',
